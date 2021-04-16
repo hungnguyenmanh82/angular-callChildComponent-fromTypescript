49da0c2653e739da12f9609295d2b28e00b7d7f8
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2021-04-16 10:20:19 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,30 @@ import { ChildComponent } from './child-component/child-component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'a13-childComponent-fromTypescript';
 
   /**
-   * 'childComponent1': là tên instant ở html của <<app-child-component>
+   * <app-child-component #childComponent1>
+   * <app-child-component #childComponent2>
+   *
+   * Với Directive cũng làm tương tự như Component đều sử dụng ClassName cả
    */
   @ViewChild('childComponent1') component1: ChildComponent;
   @ViewChild('childComponent2') component2: ChildComponent;
 
-  countClick1(){
+  countClick1() {
     this.component1.countClick();
-
   }
 
-  countClick2(){
+  countClick2() {
     this.component2.countClick();
   }
 
-
+  //cách 3 này đơn giản nhất
+  countClick3(component: ChildComponent) {
+    component.countClick();
+  }
 }
